a998b8e8f2d6a1220942678c89527fc8caaa4f90
Gabriel Pimenta <gabriel.pimenta@sistemairis.com.br>
2023-05-26 11:41:27 -0300

    ✨ [FEAT]: create Home screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme';
-import { Welcome } from './src/screens/Welcome';
+import { Home } from './src/screens/Home';
+// import { Welcome } from './src/screens/Welcome';
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Welcome />
+      {/* <Welcome /> */}
+      <Home />
     </ThemeProvider>
   );
 }
